6f3cc9243f1f822d3b27f07764c45b8b49cb488f
simplon-benoitd <benoitdelboe@hotmail.fr>
2016-02-09 14:13:32 +0100

    requêtes affichages et de modifications

--- requete-form/modifcontact.php
+++ requete-form/modifcontact.php
@@ -9,18 +9,17 @@ echo $tel;
 
 
 try {
-  $connexion=new PDO('mysql:host=localhost;dbname=simplonsite;charset=utf8','root','');
+  $connexion=new PDO('mysql:host=localhost;dbname=simplonsite;charset=utf8','root','root');
 }
   catch (Exception $e){
 die ('erreur : '.$e->getMessage());
 }
 
-       $requete="UPDATE `lien` SET `tel`= '$tel' WHERE id=1";
-echo $requete;
+       $requete="UPDATE `lien` SET `tel`='$tel' WHERE id=$iduser";
        $reponses  = $connexion->query($requete);
       //  $reponses->closeCursor();
 
-      $requete="UPDATE lien SET mail='$mail' WHERE id=1";
+      $requete="UPDATE lien SET mail='$mail' WHERE id=$iduser";
       $reponses  = $connexion->query($requete);
       $reponses->closeCursor();
 
