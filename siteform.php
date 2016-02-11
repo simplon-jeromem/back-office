6d43eb053104f181c207f63ab1eb3920e7333dbe
simplon-severinT <kambei312@hotmail.fr>
2016-02-11 15:27:00 +0100

    Update siteform.php

--- siteform.php
+++ siteform.php
@@ -32,7 +32,7 @@ if(!isset($_SESSION['iduser'])){
   $iduser = $_SESSION['iduser'];
 
   try {
-    $connexion=new PDO('mysql:host=localhost;dbname=simplonsite;charset=utf8','root','');
+    $connexion=new PDO('mysql:host=localhost;dbname=simplonsite;charset=utf8','root','root');
   }
   catch (Exception $e){
     die ('erreur : '.$e->getMessage());
