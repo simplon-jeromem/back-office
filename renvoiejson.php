248d245aedcb1e01400719e4222942ae3f639d9c
simplon-jeromem <the-3-man@live.fr>
2016-02-11 14:50:02 +0100

    Update renvoiejson.php

--- renvoiejson.php
+++ renvoiejson.php
@@ -1,4 +1,5 @@
 <?php 
+header('Access-Control-Allow-Origin:*');
  try
     {
         $bdd = new PDO('mysql:host=localhost;dbname=simplonsite;charset=utf8', 'root', 'passe');
@@ -32,7 +33,7 @@ $resultats = $bdd->query($requete);
          array_push($tableau["reseau"],$reseau);
      $id=$users["id"];
      $id=$bdd->quote($id);
-     $requete2="SELECT*FROM  `techno` INNER JOIN competences ON techno.id = competences.idt WHERE ida=$id ORDER BY niveau";
+     $requete2="SELECT*FROM  `techno` INNER JOIN competences ON techno.id = competences.idt WHERE ida=$id ORDER BY niveau desc";
      $resultats2 = $bdd->query($requete2);
      while($profil = $resultats2->fetch()){
         $competences=[
